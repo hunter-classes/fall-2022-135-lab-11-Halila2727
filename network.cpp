1fe2696acb262fa2466179ea4d9b688ee0799553
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-11-28 04:23:12 -0500

    created correct Makefile

--- network.cpp
+++ network.cpp
@@ -25,6 +25,14 @@ int Network::findID(std::string usrn)
 Network::Network()
 {
     numUsers = 0;
+
+    for(int i=0; i<MAX_USERS; i++)
+    {
+        for(int j=0; j<MAX_USERS; j++)
+        {
+            following[i][j] = false;
+        }
+    }
 }
 
 bool Network::addUser(std::string usrn, std::string dspn)
@@ -54,4 +62,25 @@ bool Network::addUser(std::string usrn, std::string dspn)
 
     profiles[numUsers] = Profile(usrn, dspn);
     numUsers++;
+    return true;
+}
+
+bool Network::follow(std::string usrn1, std::string usrn2)
+{
+    if(findID(usrn1)!=-1 && findID(usrn2)!=-1)
+    {
+        following[findID(usrn1)][findID(usrn2)] = true;
+        return true;
+    }
+    return false;
+}
+
+void Network::printDot()
+{
+    std::cout << "digraph {\n";
+    for (int i = 0; i < numUsers; i++)
+    {
+        std::cout << "\"@" << profiles[i].getUsername() << "\"\n";
+    }
+    std::cout << "\n\n";
 }
